f43ba2bce40bcaca3308e61846c0d79fa0cb50a5
Ashok kaji <ashokkaji77@gmail.com>
2025-03-24 05:56:02 +0530

    added more topics

--- first.cpp
+++ first.cpp
@@ -2,6 +2,13 @@
 using namespace std;
 
 int main(){
-    cout<< string("love").length() <<endl;
-    return 0;
+    int Age;
+    cin >> Age;
+    int Vage = 18;
+    if (Age < Vage) //Insert {} before and after cout command
+        cout << "Not old enough to vote." << endl;
+        cout << "Wait for " << (Vage - Age) << " years";     
+    else
+        cout << "Old enough to vote!";
+
 }
