fd3503c0a0c7cb95c8afe8bb0d6b62b21c599e72
Ashok kaji <ashokkaji77@gmail.com>
2025-03-25 07:19:41 +0530

    added more topics in cpp notes

--- first.cpp
+++ first.cpp
@@ -2,13 +2,15 @@
 using namespace std;
 
 int main(){
-    int Age;
-    cin >> Age;
-    int Vage = 18;
-    if (Age < Vage) //Insert {} before and after cout command
-        cout << "Not old enough to vote." << endl;
-        cout << "Wait for " << (Vage - Age) << " years";     
-    else
-        cout << "Old enough to vote!";
+    string name = "kaji";
+
+    switch (name) {
+        case "sachin":
+        cout << "sachin \n";
+        break;
+
+        case "Kaji":
+        cout << "Kaji\n";
+    }
 
 }
